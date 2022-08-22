da8449f4c1ff97c1bdbc9333c4f206d70f0a1a88
Max Siomak <pleomax250193@gmail.com>
2022-08-22 21:31:06 +0300

    Сompleted homework №6

--- js/task-09.js
+++ js/task-09.js
@@ -13,9 +13,9 @@
 //     .padStart(6, 0)}`;
 // }
 function getRandomHexColor() {
-  return `'#${Math.floor(Math.random() * 16777215)
+  return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}'`;
+    .padStart(6, 0)}`;
 }
 
 const bodyEl = document.querySelector("body");
